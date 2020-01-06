3ab73bb139aacecc4ecc6fefb52e529702e7ff1a
Jin Hyuk Kim <dev.jinddori@gmail.com>
2020-01-07 03:49:43 +0900

    end word game javascript

---
+++ common/js/word.js
@@ -0,0 +1,24 @@
+var _body = document.body;
+var _word = document.createElement('div')
+_word.textContent = '끝말잊기';
+_body.append(_word);
+
+var _input = document.createElement('input');
+var _button = document.createElement('button');
+_button.textContent = '등록'
+_body.append(_input);
+_body.append(_button);
+
+var _result = document.createElement('div');
+_body.append(_result);
+
+_button.addEventListener('click',function checkWord(){
+    if(_word.textContent[_word.textContent.length - 1] === _input.value[0]){
+        _result.textContent = 'Good';
+        _word.textContent = _input.value;
+        _input.value = '';
+        _input.focus();
+    }else{
+        _result.textContent = 'No';
+    }
+})
