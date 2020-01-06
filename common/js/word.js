6f48e16f4402cacf2de42b2f5b22aec643226f37
Jin Hyuk Kim <dev.jinddori@gmail.com>
2020-01-07 03:54:01 +0900

    master commit

--- common/js/word.js
+++ common/js/word.js
@@ -1,16 +1,17 @@
 var _body = document.body;
 var _word = document.createElement('div')
 _word.textContent = '끝말잊기';
-_body.append(_word);
+//_body.append(_word);
 
 var _input = document.createElement('input');
 var _button = document.createElement('button');
 _button.textContent = '등록'
-_body.append(_input);
-_body.append(_button);
+//_body.append(_word);
+//_body.append(_button);
 
 var _result = document.createElement('div');
-_body.append(_result);
+//_body.append(_result);
+_body.append(_word, _input, _button, _result);
 
 _button.addEventListener('click',function checkWord(){
     if(_word.textContent[_word.textContent.length - 1] === _input.value[0]){
